3abe5d38889fe19c55d0f2455f152096d4ed6dfb
vwon23 <vwon87@gmail.com>
2023-12-28 22:17:43 -0800

    add script to query from mysql

--- app_run/scripts/extract_mysql_full.py
+++ app_run/scripts/extract_mysql_full.py
@@ -1,4 +1,5 @@
 import os, sys
+import csv
 
 ## Find path of the script then find the path of parent folder and add it to system path ##
 path_script = os.path.abspath(__file__)
@@ -15,3 +16,22 @@ cf.get_current_datetime()
 logname = 'extract_mysql'
 logfile_name = f'{logname}_{cf.gvar.current_date_pst}.log'
 logger = cf.set_logger(logname, logfile_name)
+
+
+## Connect to source and extract data
+m_query = "SELECT * FROM Orders;"
+local_filename = "order_extract.csv"
+
+source_connection = cf.connect_mysql()
+m_cursor = source_connection.cursor()
+m_cursor.execute(m_query)
+results = m_cursor.fetchall()
+
+with open(local_filename, 'w') as fp:
+    csv_w = csv.writer(fp, delimiter='|')
+    csv_w.writerows(results)
+
+fp.close()
+m_cursor.close()
+source_connection.close()
+
